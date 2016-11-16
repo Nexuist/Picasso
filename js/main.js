142fd68d7fff178d97d89ea4a2ea79ef62d5cd84
Andi Andreas <nexuister@gmail.com>
2016-11-16 09:06:43 -0500

    Made upload, scan, and main view frontends

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.$ = require("./js/jquery-3.1.1.min.js");
+const {webFrame} = require("electron");
+webFrame.setZoomLevelLimits(1, 1);
 
 const supportedTypes = ["png", "jpg"];
+
 
 Vue.component("hero", {
 	template: `
@@ -11,11 +14,57 @@ Vue.component("hero", {
 				</slot>
 			</div>
 		</div>
-	</section
+	</section>
 	`
 });
 
-Vue.component("image-item", {
+Vue.component("upload-screen", {
+	data: () => {
+		return {
+			supportedTypes: ["png", "jpg"]
+		};
+	},
+	template: `
+	<hero id = "upload-screen">
+		<h1 class = "title">Drag in a folder with pictures in it.</h1>
+		<a class = "button is-dark is-outlined" onclick = "screens.screen = 'scan-screen';">
+			<span class = "icon">
+				<i class = "fa fa-folder-open"></i>
+			</span>
+			<span>Or, choose a folder</span>
+		</a>
+		<h5 class = "content upload">Supported images: {{ supportedTypes.join(", ").toUpperCase() }}</h5>
+	</hero>
+	`
+});
+
+Vue.component("scan-screen", {
+	template: `
+	<hero id = "scan-screen">
+		<progress class = "progress is-primary is-large catalog" value = "0" max = "100"></progress>
+		<p class = "content">Scanning files...</p>
+		<a class="button is-danger is-outlined" onclick = "screens.screen = 'main-screen';">
+			<span class="icon">
+				<i class="fa fa-ban"></i>
+			</span>
+			<span>Cancel</span>
+		</a>
+	</hero>`
+});
+
+Vue.component("main-screen", {
+	template: `
+	<div class="columns is-mobile" id = "main-screen">
+		<div id = "sidebar">
+			<list-item v-for = "n in 10" src = "https://placehold.it/32x32" :title = "'Image ' + n" subtitle = "Test" dimensions = "64x64"></list-item>
+		</div>
+		<div class="column" id = "content">
+			<img src = "https://placehold.it/600x600" />
+		</div>
+	</div>`
+});
+
+Vue.component("list-item", {
 	props: ["src", "title", "dimensions", "subtitle"],
 	template: `
 	<article class="media">
@@ -41,7 +90,6 @@ Vue.component("image-item", {
 let screens = new Vue({
 	el: "#screens",
 	data: {
-		screen: "main",
-		supportedTypes: supportedTypes.join(", ").toUpperCase()
+		screen: "main-screen"
 	}
 });
